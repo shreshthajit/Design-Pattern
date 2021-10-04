0fda223c8445cfb3678404bfbe4484c40ab85c89
Shreshthajit Das <43321488+shreshthajit@users.noreply.github.com>
2021-10-04 11:33:06 +0600

    Add files via upload

---
+++ Design_Pattern/Singleton Pattern/ScrabbleTestThread.java
@@ -0,0 +1,12 @@
+package Singleton;
+
+public class ScrabbleTestThread {
+    public static void main(String[] args) {
+        Runnable getTiles=new GetTheTiles();
+        Runnable getTilesAgain=new GetTheTiles();
+
+
+        new Thread(getTiles).start();
+        new Thread(getTilesAgain).start();
+    }
+}
